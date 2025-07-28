889cf7e7026551cff715346dfc9a44c41960ef2d
bjoernwolfff <bjornwolf37@gmail.com>
2025-07-28 15:12:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,13 @@ const hash = window.location.hash.substring(1);
 loadCategory(hash);
 
 function loadCategory(hash) {
-  
+  const categories = document.querySelectorAll(".category");
+  categories.forEach(cat => cat.style.display = "none");
+
+  if (hash) {
+    const target = document.getElementById(hash);
+    if (target) {
+      target.style.display = "block";
+    }
+  }
 }
